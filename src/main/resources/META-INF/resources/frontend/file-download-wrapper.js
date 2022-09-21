9972fd1b79fd18c5c9d2d1ecaad47f91415fa501
Olli Tietäväinen <ollit@vaadin.com>
2022-09-21 09:44:03 +0300

    WIP lit transformation

--- src/main/resources/META-INF/resources/frontend/file-download-wrapper.js
+++ src/main/resources/META-INF/resources/frontend/file-download-wrapper.js
@@ -1,26 +1,24 @@
-import '@polymer/polymer/polymer-legacy.js';
-import { html } from '@polymer/polymer/lib/utils/html-tag.js';
-import { PolymerElement } from '@polymer/polymer/polymer-element.js';
-class FileDownloadWrapper extends PolymerElement {
-  static get template() {
-    return html`
-   <a id="download-link"></a> 
-`;
+import { html, LitElement, css } from "lit";
+import "@polymer/polymer/polymer-legacy.js";
+
+class FileDownloadWrapper extends LitElement {
+  render() {
+    return html` <a id="download-link"></a> `;
   }
 
-  _attachDom(dom) {
-      this.appendChild(dom);
+  createRenderRoot() {
+    // Do not use a shadow root
+    return this;
   }
 
   static get is() {
-      return 'file-download-wrapper';
+    return "file-download-wrapper";
   }
 
   static get properties() {
-      return {
-          // Declare your properties here.
-      };
+    return {
+      // Declare your properties here.
+    };
   }
 }
 customElements.define(FileDownloadWrapper.is, FileDownloadWrapper);
-
